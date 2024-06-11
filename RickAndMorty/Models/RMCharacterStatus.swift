a2bc8a9293b882600111aef7f1862999cc3641e3
bilgehanakyol <bilgehanakyol12@hotmail.com>
2024-06-11 20:40:27 +0300

    create RMCharacterGender, RMCharacterStatus, RMOrigin and RMSingleLocation files

---
+++ RickAndMorty/Models/RMCharacterStatus.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterStatus.swift
+//  RickAndMorty
+//
+//  Created by Bilgehan Akyol on 11.06.2024.
+//
+
+import Foundation
---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,8 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Bilgehan Akyol on 11.06.2024.
+//
+
+import Foundation

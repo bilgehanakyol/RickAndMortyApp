453f0c97441d5a9044a6f6ef5826fb487eff9ce1
bilgehanakyol <bilgehanakyol12@hotmail.com>
2024-06-11 12:32:07 +0300

    add mvvm struct and files. Create controller files and RMModel files

---
+++ RickAndMorty/Models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  Location.swift
+//  RickAndMorty
+//
+//  Created by Bilgehan Akyol on 11.06.2024.
+//
+
+import Foundation
--- RickAndMorty/Resources/SceneDelegate.swift
+++ RickAndMorty/Resources/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScence = (scene as? UIWindowScene) else { return }
         
-        let vc = ViewController()
+        let vc = RMTabBarController()
         let window = UIWindow(windowScene: windowScence)
         
         window.rootViewController = vc

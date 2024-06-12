f1e8bd1f7c35db61d30f103479226fdb83326e56
bilgehanakyol <bilgehanakyol12@hotmail.com>
2024-06-12 12:25:20 +0300

    last thing is create API request. add all files.

--- RickAndMorty/APIClient/RMEndpoint.swift
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+/// Represents uniqe API endpoint
+@frozen enum RMEndpoint: String {
+    /// Endpoint to get character info
+    case character
+    /// Endpoint to get location info
+    case location
+    /// Endpoint to get episode info
+    case episode
+}
--- RickAndMorty/Controllers/Core/RMCharacterViewController.swift
+++ RickAndMorty/Controllers/Core/RMCharacterViewController.swift
@@ -7,23 +7,18 @@
 
 import UIKit
 
-class RMCharacterViewController: UIViewController {
+/// Controller to show and search for Locations
+final class RMCharacterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        title = "Characters"
+        
+        let request = RMRequest(
+            endpoint: .character
+        )
+        print(request.url)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

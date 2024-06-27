cc51f7cb8e8794851844cf8b86046c7db253f24e
bilgehanakyol <bilgehanakyol12@hotmail.com>
2024-06-27 16:56:10 +0300

    fetch episodes

--- RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
+++ RickAndMorty/ViewModels/CharacterDetail/RMCharacterEpisodeCollectionViewCellViewModel.swift
@@ -7,10 +7,60 @@
 
 import Foundation
 
+protocol RMEpisodeDataRender {
+    var name: String { get }
+    var air_date: String { get }
+    var episode: String { get }
+}
+
 final class RMCharacterEpisodeCollectionViewCellViewModel {
     private let episodeDatUrl: URL?
+    private var isFetching: Bool = false
+    private var dataBlock: ((RMEpisodeDataRender) -> Void)?
+    
+    private var episode: RMEpisode? {
+        didSet {
+            guard let model = episode else {
+                return
+            }
+            dataBlock?(model)
+        }
+    }
+    
+    //MARK: - Init
     
     init(episodeDataUrl: URL?) {
         self.episodeDatUrl = episodeDataUrl
+    }
+    
+    //MARK: - Public
+    
+    public func RegisterForData(_ block: @escaping (RMEpisodeDataRender) -> Void) {
+        self.dataBlock = block
+    }
+    
+    public func fetchEpisode() {
+        guard !isFetching else {
+            if let model = episode {
+                dataBlock?(model)
+            }
+            return
+        }
+        guard let url = episodeDatUrl,
+              let rmRequest = RMRequest(url: url) else {
+            return
+        }
+        isFetching = true
+        
+        RMService.shared.execute(rmRequest, expecting: RMEpisode.self) { [weak self] result in
+            switch result {
+            case .success(let model):
+                DispatchQueue.main.async {
+                    self?.episode = model
+                }
+            case .failure(let failure):
+                print(String(describing: failure))
+            }
+        }
     }
 }
--- RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
+++ RickAndMorty/Views/CharacterDetails/RMCharacterEpisodeCollectionViewCell.swift
@@ -12,6 +12,8 @@ final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        contentView.backgroundColor = .systemBlue
+        contentView.layer.cornerRadius = 8
     }
     
     required init?(coder: NSCoder) {
@@ -27,7 +29,12 @@ final class RMCharacterEpisodeCollectionViewCell: UICollectionViewCell {
     }
     
     public func configure(with viewModel: RMCharacterEpisodeCollectionViewCellViewModel) {
-        
+        viewModel.RegisterForData { data in
+            print(data.name)
+            print(data.episode)
+            print(data.air_date)
+        }
+        viewModel.fetchEpisode()
     }
 
 }

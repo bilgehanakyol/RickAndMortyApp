654bed51a24bdfc262127a50246345ac9ff4e436
bilgehanakyol <bilgehanakyol12@hotmail.com>
2024-07-09 12:48:18 +0300

    add icon and launch screen. Start building out Locations tab.

---
+++ RickAndMorty/ViewModels/RMLocationViewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocationViewViewModel.swift
+//  RickAndMorty
+//
+//  Created by Bilgehan Akyol on 9.07.2024.
+//
+
+import Foundation
---
+++ RickAndMorty/Views/Location/RMLocationView.swift
@@ -0,0 +1,20 @@
+//
+//  RMLocationView.swift
+//  RickAndMorty
+//
+//  Created by Bilgehan Akyol on 9.07.2024.
+//
+
+import UIKit
+
+class RMLocationView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
